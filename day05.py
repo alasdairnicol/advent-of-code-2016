2ae117f90493095748000150cfb06d7619210292
Alasdair Nicol <alasdair@thenicols.net>
2022-04-25 23:02:10 +0100

    Day 5 parts 1 and 2

---
+++ day05.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+import itertools
+import hashlib
+from typing import Generator
+from collections import Counter
+
+
+def complete(part_a, part_b):
+    return len(part_a) == 8 and "_" not in part_b
+
+
+def main():
+    door_id = read_input()
+
+    x = 0
+
+    part_a = []
+    part_b = ["_"] * 8
+
+    while True:
+        trial = f"{door_id}{x}".encode("utf-8")
+        digest = hashlib.md5(trial).hexdigest()
+        if digest.startswith("00000"):
+            # Update
+            if len(part_a) < 8:
+                part_a.append(digest[5])
+            try:
+                if part_b[int(digest[5])] == "_":
+                    part_b[int(digest[5])] = digest[6]
+            except (IndexError, ValueError):
+                pass
+            if complete(part_a, part_b):
+                break
+
+        x += 1
+
+    part_a = "".join(part_a)
+    part_b = "".join(part_b)
+
+    print(f"{part_a=}")
+    print(f"{part_b=}")
+
+
+def read_input() -> str:
+    with open("day05.txt") as f:
+        return f.read().strip()
+
+
+if __name__ == "__main__":
+    main()

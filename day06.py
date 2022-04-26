a4e55c32fe25eade07f563912a10cf30d08b47ab
Alasdair Nicol <alasdair@thenicols.net>
2022-04-26 18:07:15 +0100

    Day 6 parts 1 and 2

---
+++ day06.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import itertools
+import hashlib
+from typing import Generator
+from collections import Counter
+
+
+def main():
+    columns = read_input()
+
+    counters = (Counter(column) for column in columns)
+    most_common = [counter.most_common() for counter in counters]
+
+    part_a = "".join(x[0][0] for x in most_common)
+    part_b = "".join(x[-1][0] for x in most_common)
+
+    print(f"{part_a=}")
+    print(f"{part_b=}")
+
+
+def read_input() -> Generator[int, None, None]:
+    with open("day06.txt") as f:
+        return zip(*(line.strip() for line in f.readlines()))
+
+
+if __name__ == "__main__":
+    main()

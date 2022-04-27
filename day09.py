d6165d3295458fc2d6f7d6e46567690e2e6a20c5
Alasdair Nicol <alasdair@thenicols.net>
2022-04-27 08:40:04 +0100

    Day 9 part 2

--- day09.py
+++ day09.py
@@ -3,12 +3,14 @@ from typing import Generator
 
 
 def main():
-    input_string = list(read_input())
+    input_string = str(read_input())
 
     part_a = do_part_a(input_string)
     print(f"{part_a=}")
 
-    print(f"part_b:")
+    # input_string = '(3x3)XYZ'
+    part_b = expanded_length(input_string)
+    print(f"{part_b=}")
 
 
 def do_part_a(input_string):
@@ -37,6 +39,34 @@ def do_part_a(input_string):
             output_string += x
 
     return len(output_string)
+
+
+def expanded_length(input_string):
+    # FIXME use better names than length and num!
+    input_string = iter(input_string)
+
+    length = 0
+    while True:
+        try:
+            x = next(input_string)
+        except StopIteration:
+            break
+
+        if x == "(":
+            marker = ""
+            while True:
+                y = next(input_string)
+                if y == ")":
+                    break
+                marker += y
+
+            num, repeat = (int(x) for x in marker.split("x"))
+
+            letters = "".join(next(input_string) for x in range(num))
+            length += expanded_length(letters) * repeat
+        else:
+            length += 1
+    return length
 
 
 def read_input() -> Generator[int, None, None]:

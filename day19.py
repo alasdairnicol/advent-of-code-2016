697525d5b6928f446fe8c503329a3b962177f4c7
Alasdair Nicol <alasdair@thenicols.net>
2022-06-04 15:26:28 +0100

    Calculate day 19 part 2 much faster

--- day19.py
+++ day19.py
@@ -1,22 +1,23 @@
 #!/usr/bin/env python
 
 
-def do_part_a(num_elves):
-    def next_round(elves):
-        """Go round the circle once, and return the new list of elves"""
-        # If there are an odd number of elves, then the last elf will
-        # be at the start of the next list
-        last_place = [elves[-1]] if len(elves) % 2 else []
-        return last_place + elves[0:-1:2]
+def next_round_a(elves):
+    """Go round the circle once, and return the new list of elves"""
+    # If there are an odd number of elves, then the last elf will
+    # be at the start of the next list
+    last_place = [elves[-1]] if len(elves) % 2 else []
+    return last_place + elves[0:-1:2]
 
+
+def do_part_a(num_elves):
     elves = list(range(1, num_elves + 1))
     while len(elves) > 1:
-        elves = next_round(elves)
+        elves = next_round_a(elves)
     return elves[0]
 
 
-def do_part_b(num_elves):
-    # FIXME This brute force approach is slow!
+def brute_force_part_b(num_elves):
+    """Warning: This brute force approach is slow!"""
     elves = list(range(1, num_elves + 1))
     position = 0
 
@@ -32,19 +33,29 @@ def do_part_b(num_elves):
     return elves[0]
 
 
+def do_part_b(num_elves):
+    power_of_three = 1
+    while 3 * power_of_three <= num_elves:
+        power_of_three *= 3
+
+    diff = num_elves - power_of_three
+
+    if diff == 0:
+        return num_elves
+    elif diff <= power_of_three:
+        return diff
+    else:
+        return 2 * num_elves - 3 * power_of_three
+
+
 def main():
     num_elves = read_input()
 
     part_a = do_part_a(num_elves)
     print(f"{part_a=}")
 
-    # part_b = do_part_b(num_elves)
-    # print(f"{part_b=}")
-
-    # Look for the pattern
-    for x in range(1, 100001):
-        if x == do_part_b(x):
-            print(x, do_part_b(x))
+    part_b = do_part_b(num_elves)
+    print(f"{part_b=}")
 
 
 def read_input() -> int:
